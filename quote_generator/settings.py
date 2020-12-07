cfd79cb45637e727a203bb62a352f88e508221b5
Timothy Angus <timangus321@gmail.com>
2020-12-07 16:03:18 +0100

    prepare for deployment

--- quote_generator/settings.py
+++ quote_generator/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['quote-generator-finder.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['quote-generator-finder.herokuapp.com']
 
 # Application definition
 
